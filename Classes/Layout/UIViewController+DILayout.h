443cce6489d47f94d90bf153e6930e7f2a1aaf98
BackYu <szyhf@qq.com>
2016-05-23 16:26:29 +0800

    修复了对UIKit的依赖，删除的多于的声明。

--- Classes/Layout/UIViewController+DILayout.h
+++ Classes/Layout/UIViewController+DILayout.h
@@ -9,17 +9,4 @@
 #import <UIKit/UIKit.h>
 
 @interface UIViewController (DILayout)
--(void)left:(NSString*)formula;
--(void)right:(NSString*)formula;
--(void)top:(NSString*)formula;
--(void)bottom:(NSString*)formula;
-
--(void)height:(NSString*)formula;
--(void)width:(NSString*)formula;
-
--(void)centerX:(NSString*)formula;
--(void)centerY:(NSString*)formula;
-
--(void)leading:(NSString*)formula;
--(void)trailing:(NSString*)formula;
 @end
