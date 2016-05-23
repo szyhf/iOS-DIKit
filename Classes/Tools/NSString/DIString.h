03fa0f79070d668a1dbb2cbb3807e56a627d5df6
BackYu <szyhf@qq.com>
2016-05-23 16:11:23 +0800

    统一重命名了DIString的头文件。

--- Classes/Tools/NSString/DIString.h
+++ Classes/Tools/NSString/DIString.h
@@ -8,8 +8,9 @@
 
 #ifndef NSString_DIAdd_h
 #define NSString_DIAdd_h
-#import"NSString+Json.h"
-#import"NSString+Judge.h"
+#import "NSString+Json.h"
+#import "NSString+Judge.h"
 #import "NSString+Additions.h"
 #import "NSString+Crypto.h"
+#import "NSString+RegularExpression.h"
 #endif /* NSString_DIAdd_h */
