72c109525d540af8ec8751ca2011f22db058df89
BackYu <szyhf@qq.com>
2016-05-24 10:53:09 +0800

    新增了Runtimes的工具，允许通过字符串直接调用函数（包括公开与私有的）

---
+++ Classes/Tools/Object/NSObject+Runtimes.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+Runtimes.h
+//  DIKit
+//
+//  Created by Back on 16/5/24.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Runtimes)
+
+@end
---
+++ Classes/Tools/Object/NSObject+Runtimes.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+Runtimes.m
+//  DIKit
+//
+//  Created by Back on 16/5/24.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import "NSObject+Runtimes.h"
+
+@implementation NSObject (Runtimes)
+
+@end
