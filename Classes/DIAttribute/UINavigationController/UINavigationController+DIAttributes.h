f0e61c876ac7c6f924a90c32b745b223637c7d6b
BackYu <szyhf@qq.com>
2016-06-08 00:40:21 +0800

    新增UINavigationController的DIAttribute，现在支持模板属性push，用于控制顺序视图。

---
+++ Classes/DIAttribute/UINavigationController/UINavigationController+DIAttributes.h
@@ -0,0 +1,14 @@
+//
+//  UINavigationController+DIAttributes.h
+//  DIKit
+//
+//  Created by Back on 16/6/7.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NSObject+DIAttribute.h"
+
+@interface UINavigationController (DIAttributes)
+
+@end
---
+++ Classes/DIAttribute/UINavigationController/UINavigationController+DIAttributes.m
@@ -0,0 +1,67 @@
+//
+//  UINavigationController+DIAttributes.m
+//  DIKit
+//
+//  Created by Back on 16/6/7.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import "UINavigationController+DIAttributes.h"
+#import "DITree.h"
+#import "DIRouter.h"
+#import "DITemplateNode.h"
+
+@implementation UINavigationController (DIAttributes)
++(UndefinedKeyHandlerBlock)di_AttributeBlock:(NSString*)key
+{
+	static NSDictionary<NSString*,UndefinedKeyHandlerBlock>* _instance;
+	static dispatch_once_t _token;
+	dispatch_once(&_token,
+				  ^{
+					  _instance = @{
+									@"push":self.pushKey
+									} ;
+				  });
+	return _instance[key];
+}
++(UndefinedKeyHandlerBlock)pushKey
+{
+	return ^void(UINavigationController* _self,NSString*key,id value)
+	{
+		NSMutableArray* pushs = [_self valueForKey:@"di_push"];
+		NSArray* newAry;
+		if([value isKindOfClass:NSArray.class])
+		{
+			newAry = value;
+		}
+		else
+		{
+			newAry=@[value];
+		}
+		
+		if(pushs)
+		{
+			[pushs addObjectsFromArray:newAry];
+		}
+		else
+		{
+			pushs = [NSMutableArray arrayWithArray:newAry];
+		}
+		[_self setValue:pushs forKey:@"di_push"];
+		
+		if([[_self childViewControllers]count]==0)
+			[_self pushNext];
+	};
+}
+-(void)pushNext
+{
+	NSArray* pushs = [self valueForKey:@"di_push"];
+	NSInteger currentIndex = [[self childViewControllers]count];
+	if(pushs.count>currentIndex)
+	{
+		DITemplateNode* targetNode = pushs[currentIndex];
+		UIViewController* ctrl =targetNode.newImplement;
+		[self pushViewController:ctrl animated:YES];
+	}
+}
+@end
