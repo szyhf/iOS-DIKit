21922685eb54f4e9b2f6be69c298808d1058da0d
BackYu <szyhf@qq.com>
2016-06-08 00:44:57 +0800

    更新了DINode+awake的工作逻辑，现在支持生成实例以及根据代理节点定义实现节点的分文件定义。

--- Classes/DITree/DINode/DINode+Awake.m
+++ Classes/DITree/DINode/DINode+Awake.m
@@ -11,11 +11,17 @@
 #import "DIRouter+HandlerBlocks.h"
 #import "DIRouter+Assemble.h"
 #import "DIContainer.h"
+#import "DITree.h"
 
 @implementation DINode (Awake)
 #pragma DINodeAwakeProtocol
 -(void)awake
 {
+	if(self.implement && self.isGlobal)
+	{
+		return;
+	}
+	
 	[self beforeImply];
 	[self implying];
 	[self afterImply];
@@ -25,6 +31,41 @@
 		[self finishAll];
 	}
 }
+
+-(void)beforeImply
+{
+	for (DINode* child in self.children)
+	{
+		[child awake];
+	}
+}
+
+-(void)implying
+{
+	DINode* detailRootNode = DITree.instance.nameToNode[self.name];
+	if(detailRootNode && self!=detailRootNode )
+	{
+		//如果当前节点有细化的实现，则以细化实现为基础更新
+		[detailRootNode awake];
+		self.implement = detailRootNode.implement;
+	}
+	else
+	{
+		self.implement = [self makeInstance];
+	}
+		
+}
+
+-(void)afterImply
+{
+	for (DINode* child in self.children)
+ 	{
+		[child assemblyTo:self];
+	}
+	
+	[self patchAttribute];
+}
+
 -(void)finishAll
 {
 	[self delay];
@@ -44,29 +85,6 @@
 	
 	[self.delayBlocks removeAllObjects];//释放。。
 }
--(void)beforeImply
-{
-	for (DINode* child in self.children)
-	{
-		[child awake];
-	}
-}
-
--(void)implying
-{
-	self.implement = [self makeInstance];
-}
-
--(void)afterImply
-{
-	for (DINode* child in self.children)
- 	{
-		[child assemblyTo:self];
-	}
-	
-	[self patchAttribute];
-}
-
 -(void)assemblyTo:(DINode*)parentNode
 {
 	//处理默认的组装
@@ -81,20 +99,48 @@
 
 -(void)patchAttribute
 {
-	[self autoSetNUIClass:self.name withInstance:self.implement];
+	[self autoSetNUIClass];
 	[self.implement updateByNode:self];
 }
 
--(void)autoSetNUIClass:(NSString*)element withInstance:(id)instance
+-(void)autoSetNUIClass
 {
+	//如果已经手动设置了则跳过。
+	if([[self.implement valueForKeyPath:@"style"] isKindOfClass:NSString.class])
+		return;
+	
+	//如果节点中已经设置则跳过
+	if(self.attributes[@"style"])
+		return;
+	
+	//优先按照name匹配，匹配失败则尝试使用classname匹配。
 	NUISettings*nuiInstance = [NUISettings invokeMethod:@"getInstance"];
-	if(nuiInstance.styles[element]!=nil)
+
+	if(nuiInstance.styles[self.name])
 	{
-		if([NSString isNilOrEmpty:[instance valueForKey:@"nuiClass"]])
+		self.attributes[@"style"] = self.name;
+	}
+	else if(nuiInstance.styles[self.className])
+	{
+		self.attributes[@"style"] = self.className;
+	}
+	else
+	{
+		//递归监测是否存在父类的定义
+		Class currentClass = self.clazz;
+		while(currentClass!=nil)
 		{
-			[instance setValue:element forKey:@"nuiClass"];
+			NSString* currentClassName = NSStringFromClass(currentClass);
+			if(nuiInstance.styles[currentClassName])
+			{
+				self.attributes[@"style"] = currentClassName;
+				break;
+			}
+			currentClass = [currentClass superclass];
 		}
 	}
+	//如果还配不到就按NUI的默认处理。
+	return;
 }
 
 -(id)makeInstance
@@ -102,7 +148,7 @@
 	id nodeInstance;
 	if([self isGlobal])
 	{
-		nodeInstance = [DIContainer getInstance:self.clazz];
+		nodeInstance = [DIContainer makeInstance:self.clazz];
 		[DIContainer setAlias:self.name forInstance:nodeInstance];
 	}
 	else
