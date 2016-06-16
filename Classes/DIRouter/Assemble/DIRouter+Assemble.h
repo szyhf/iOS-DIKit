320770ce63b41f0787e9c369f229b22d7e08d5b9
Back Yu <szyhf@163.com>
2016-06-16 14:47:04 +0800

    删除了addElementToParent的方法

--- Classes/DIRouter/Assemble/DIRouter+Assemble.h
+++ Classes/DIRouter/Assemble/DIRouter+Assemble.h
@@ -11,15 +11,6 @@
 #import "DIRouter+HandlerBlocks.h"
 
 @interface DIRouter (Assemble)
-/**
- *  将指定的元素添加到制定的父元素
- *
- *  @param element     子元素名
- *  @param lastElement 父元素名
- */
-+(void)addElement:(NSString*)element
-		 toParent:(NSString*)lastElement;
-
 +(RealizeHandlerBlock)blockToAddElement:(NSString*)element
 							   toParent:( NSString*)lastElement;
 @end
