0c9a9496bb86005de0278e626e2dc356218f249e
Back Yu <szyhf@163.com>
2016-06-21 02:45:31 +0800

    新增从NSNumber到格式化时间的扩展。

---
+++ Classes/Tools/NSNumber/NSNumber+DITimeFormater.h
@@ -0,0 +1,13 @@
+//
+//  NSNumber+DITimeFormater.h
+//  DIKit
+//
+//  Created by Back on 16/6/17.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (DITimeFormater)
+-(NSString*)toTimeString;
+@end
---
+++ Classes/Tools/NSNumber/NSNumber+DITimeFormater.m
@@ -0,0 +1,57 @@
+//
+//  NSNumber+DITimeFormater.m
+//  DIKit
+//
+//  Created by Back on 16/6/17.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import "NSNumber+DITimeFormater.h"
+
+@implementation NSNumber (DITimeFormater)
+-(NSString*)toTimeString
+{
+	return [self toTimeStringFrom:[NSDate date]];
+}
+-(NSString*)toTimeStringFrom:(NSDate*)from
+{
+	NSUInteger toTime = [self unsignedIntegerValue];
+	NSUInteger fromTime = [from timeIntervalSince1970];
+	NSString* suffix;
+	
+	NSUInteger diff =0;
+	
+	if(fromTime>toTime)
+	{
+		suffix = @"后";
+		diff = fromTime-toTime;
+	}
+	else
+	{
+		suffix = @"前";
+		diff = toTime-fromTime;
+	}
+	
+	if(diff<3600)
+	{
+		NSUInteger minDif = diff/60;
+		return [NSString stringWithFormat:@"%ld分钟%@",minDif,suffix];
+	}
+	
+	if(diff<86400)
+	{
+		NSUInteger hourDif = diff/3600;
+		return [NSString stringWithFormat:@"%ld小时%@",hourDif,suffix];
+	}
+	
+	static NSDateFormatter *formatter ;
+	if(!formatter)
+	{
+		formatter = [[NSDateFormatter alloc] init];
+		[formatter dateFromString:@"YYYY-MM-DD HH:mm:ss"];
+	}
+	
+	
+	return [formatter stringFromDate:[NSDate dateWithTimeIntervalSince1970:toTime]];
+}
+@end
