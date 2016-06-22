9750cb5342418c21b28163a729caf7c0b3fdcfab
Back Yu <szyhf@163.com>
2016-06-22 15:44:34 +0800

    新增DITabBar

---
+++ Classes/DITouch/DITabBar/DITabBar.h
@@ -0,0 +1,13 @@
+//
+//  DITabBar.h
+//  DIKit
+//
+//  Created by Back on 16/6/22.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DITabBar : UITabBar
+@property (nonatomic, strong) NSString* heightRate;
+@end
---
+++ Classes/DITouch/DITabBar/DITabBar.m
@@ -0,0 +1,13 @@
+//
+//  DITabBar.m
+//  DIKit
+//
+//  Created by Back on 16/6/22.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import "DITabBar.h"
+
+@implementation DITabBar
+
+@end
