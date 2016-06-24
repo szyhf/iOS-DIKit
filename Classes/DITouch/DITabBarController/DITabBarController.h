ee5949e819a97cdd03437700750cf1be2f3e6acd
Back Yu <szyhf@163.com>
2016-06-24 17:30:40 +0800

    新增DITabBarController

---
+++ Classes/DITouch/DITabBarController/DITabBarController.h
@@ -0,0 +1,12 @@
+//
+//  DITabBarController.h
+//  Liangfeng
+//
+//  Created by Back on 16/6/18.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DITabBarController : UITabBarController
+@end
---
+++ Classes/DITouch/DITabBarController/DITabBarController.m
@@ -0,0 +1,51 @@
+//
+//  DITabBarController.m
+//  Liangfeng
+//
+//  Created by Back on 16/6/18.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import "DITabBarController.h"
+#import "UIColor+Wonderful.h"
+#import "Masonry.h"
+#import "UILabel+Wonderful.h"
+#import "UIImage+Resize.h"
+@interface DITabBarController()
+@end
+
+@implementation DITabBarController
+-(void)viewDidAppear:(BOOL)animated
+{
+	[super viewDidAppear:animated];
+	CGFloat screenHeight=[UIScreen mainScreen].bounds.size.height;
+	NSString* heightRateString = [self.tabBar valueForKey:@"heightRate"];
+	CGFloat heightRate = [heightRateString doubleValue];
+	if(heightRate>0)
+	{
+		CGFloat tabBarHeight = screenHeight * heightRate;
+		
+		CGRect tabFrame = self.tabBar.frame; //self.TabBar is IBOutlet of your TabBar
+		tabFrame.size.height = tabBarHeight;
+		tabFrame.origin.y = self.view.frame.size.height - tabBarHeight;
+		self.tabBar.frame = tabFrame;
+	}
+
+	CGFloat fixedHeight = 30;//*[UIScreen mainScreen].scale;
+		
+	for (UITabBarItem* item in self.tabBar.items)
+	{
+		UIImage* barImage = item.image;
+		UIImage* barSelectedImage = item.selectedImage;
+		if(barImage!=nil)
+		{
+			item.image = [barImage resizedImageToFitInHeight:fixedHeight];
+		}
+		if(barSelectedImage!=nil)
+		{
+			item.selectedImage = [barSelectedImage resizedImageToFitInHeight:fixedHeight];
+		}
+	}
+}
+
+@end
