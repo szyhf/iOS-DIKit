e1d076cdae915c22944141e8134611d567570602
Back Yu <szyhf@163.com>
2016-06-27 17:59:57 +0800

    替换reg的方法中关于setValue的方式。

--- Classes/DITree/DIRegisterNode/DIRegisterNode+Awake.m
+++ Classes/DITree/DIRegisterNode/DIRegisterNode+Awake.m
@@ -7,11 +7,12 @@
 //
 
 #import "DIRegisterNode.h"
+#import <objc/runtime.h>
 
 @implementation DIRegisterNode (Awake)
 -(void)assemblyTo:(DINode*)parentNode
 {
 	[super assemblyTo:parentNode];//完成默认组装以后设置为其属性。
-	[parentNode.implement setValue:self.implement forKeyPath:self.registry];
+	objc_setAssociatedObject(parentNode.implement, NSSelectorFromString(self.registry), self.implement, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 @end
