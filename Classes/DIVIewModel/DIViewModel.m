2533503002cc4ed36e6105bc77581f0f3e821fa8
Back Yu <szyhf@163.com>
2016-06-29 20:18:45 +0800

    修复了DIVM的一个Bug，现在重置BindingInstance的时候会强制重置observer（其实还是不太好但总还能用）

--- Classes/DIVIewModel/DIViewModel.m
+++ Classes/DIVIewModel/DIViewModel.m
@@ -23,6 +23,8 @@
 
 -(void)setBindingInstance:(NSObject*)bindingInstance
 {
+	//重置观察，要不ViewModel在重用时可能无法正确更新被观察的对象
+	[self.KVOControllerNonRetaining unobserveAll];
 	[self watchModel:bindingInstance named:@"target"];
 }
 
