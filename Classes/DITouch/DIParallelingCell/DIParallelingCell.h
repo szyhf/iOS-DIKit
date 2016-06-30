6a649671f9240b728c91eda3c763225daa558c24
Back Yu <szyhf@163.com>
2016-06-30 21:47:12 +0800

    新增DIParallelingCell（未完成）

---
+++ Classes/DITouch/DIParallelingCell/DIParallelingCell.h
@@ -0,0 +1,13 @@
+//
+//  DIParallelingCell.h
+//  Pods
+//
+//  Created by Back on 16/6/30.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DIParallelingCell : UITableViewCell
+@property (nonatomic, strong) NSArray* group;
+@end
---
+++ Classes/DITouch/DIParallelingCell/DIParallelingCell.m
@@ -0,0 +1,13 @@
+//
+//  DIParallelingCell.m
+//  Pods
+//
+//  Created by Back on 16/6/30.
+//
+//
+
+#import "DIParallelingCell.h"
+
+@implementation DIParallelingCell
+
+@end
