69bdb0cd7c705b7ce436b5b89f8265af557ee3eb
Back Yu <szyhf@163.com>
2016-07-05 18:03:29 +0800

    更新了注释

--- Classes/DIModel/DIDictionaryProxyModel/DIDictionaryProxyModel.h
+++ Classes/DIModel/DIDictionaryProxyModel/DIDictionaryProxyModel.h
@@ -10,7 +10,7 @@
 /**
  *  快速实现一个如下结构的集合对象的监视代理
  *  @interface XXModels
- *  @property (nonatomic, strong) NSDictionary<NSString*,XXModel*>* xxModels;
+ *  @property (nonatomic, strong) NSDictionary<NSString* primaryKeyValue,XXModel*>* xxModels;
  *  @end
  *  字典的键值即该Model的唯一索引或者主键，可以唯一查找该Model。
  *  该结构主要用于解决关联式的模型组合
