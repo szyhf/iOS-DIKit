4ae22934f99ed5d730455800250bcb3e63a6796c
Back Yu <szyhf@163.com>
2016-07-14 17:15:17 +0800

    增加了一些注释。

--- Classes/DIVIewModel/DITableViewModel/DITableVIewModel.h
+++ Classes/DIVIewModel/DITableViewModel/DITableVIewModel.h
@@ -7,8 +7,20 @@
 //
 
 #import "DIViewModel.h"
-
+/**
+ *  需要与合适的单元格VM配套使用
+ */
 @interface DITableViewModel : DIViewModel
+/**
+ *  单元格的VM的类型名
+ *
+ *  @return 单元格的类型名
+ */
 +(Class)cellViewModelClass;
+/**
+ *  该表格唯一性的DIArrayModel类型的集合
+ *
+ *  @return 
+ */
 +(Class)tableModelWatchClass;
 @end
