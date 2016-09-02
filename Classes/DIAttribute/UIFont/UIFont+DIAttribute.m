bc3cfa4151221806446940b6024a3ed0cb4e79c6
Back Yu <szyhf@163.com>
2016-09-02 11:30:15 +0800

    暂时注释不完善的UIFont

--- Classes/DIAttribute/UIFont/UIFont+DIAttribute.m
+++ Classes/DIAttribute/UIFont/UIFont+DIAttribute.m
@@ -10,28 +10,33 @@
 #import "DIObject.h"
 
 @implementation UIFont (DIAttribute)
-+(void)load
-{
-	[self exchangeClassSelector:@selector(systemFontOfSize:) toSelector:@selector(di_systemFontOfSize:)];
-	//[self exchangeClassSelector:@selector(systemFontOfSize:weight:) toSelector:@selector(di_systemFontOfSize:weight:)];
-	[self exchangeClassSelector:@selector(boldSystemFontOfSize:) toSelector:@selector(di_boldSystemFontOfSize:)];
-	//[self exchangeClassSelector:@selector(italicSystemFontOfSize:) toSelector:@selector(di_italicSystemFontOfSize:)];
-}
 
-+(UIFont*)di_systemFontOfSize:(CGFloat)fontSize
-{
-	return [self fontWithName:@"STYuanti-SC-Regular" size:fontSize];
-}
+/**
+ *   未完成。
+ */
+
+//+(void)load
+//{
+	//[self exchangeClassSelector:@selector(systemFontOfSize:) toSelector:@selector(di_systemFontOfSize:)];
+	////[self exchangeClassSelector:@selector(systemFontOfSize:weight:) toSelector:@selector(di_systemFontOfSize:weight:)];
+	//[self exchangeClassSelector:@selector(boldSystemFontOfSize:) toSelector:@selector(di_boldSystemFontOfSize:)];
+	////[self exchangeClassSelector:@selector(italicSystemFontOfSize:) toSelector:@selector(di_italicSystemFontOfSize:)];
+//}
+
+//+(UIFont*)di_systemFontOfSize:(CGFloat)fontSize
+//{
+	//return [self fontWithName:@"STYuanti-SC-Regular" size:fontSize];
+//}
 
 //+(UIFont*)di_systemFontOfSize:(CGFloat)fontSize weight:(CGFloat)weight
 //{
 	//return [self fontWithName:@"" size:fontSize];
 //}
 
-+(UIFont*)di_boldSystemFontOfSize:(CGFloat)fontSize
-{
-	return [self fontWithName:@"STYuanti-SC-Bold" size:fontSize];
-}
+//+(UIFont*)di_boldSystemFontOfSize:(CGFloat)fontSize
+//{
+	//return [self fontWithName:@"STYuanti-SC-Bold" size:fontSize];
+//}
 
 //+(UIFont*)di_italicSystemFontOfSize:(CGFloat)fontSize
 //{
